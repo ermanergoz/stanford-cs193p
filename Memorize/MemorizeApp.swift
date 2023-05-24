114213a157d31d2af3b4e7affaa2bdbd39751e83
Yusuf Erman ERGÖZ <yusuf.ergoz@wundermanthompson.com>
2023-05-24 15:52:40 +0200

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Yusuf Ergoz on 2023. 05. 24..
+//
+
+import SwiftUI
+
+@main
+class MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
