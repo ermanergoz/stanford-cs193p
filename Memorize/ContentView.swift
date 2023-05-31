ebbfe1b790258d666036687b41aa23315167f487
Yusuf Erman ERGÖZ <yusuf.ergoz@wundermanthompson.com>
2023-05-31 13:44:08 +0200

    Lecture 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    /*
+     var body: some /*we are telling the compiler that the type is some View, figure out what it actually is. In this case, it is a Text so it will be replaced with Text. It is called opaque type*/ View { //not stored in the memory. The value of body is being calculated by this function
+     /*there is a hidden return here*/
+     Text("Hello, world!")
+     .foregroundColor(Color.blue)
+     .padding([.leading, .bottom, .trailing], 8.0) //This function exists for all structs that behave like a view. When we use this function, it no longer returns Text object but it returns ModifiedContent something something
+     RoundedRectangle(cornerRadius: 25).stroke(lineWidth: 3).foregroundColor(Color.red).padding(20)
+     
+     }
+     */
+    /*
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+        return ZStack(alignment: .center, content: {
             Text("Hello, world!")
+                .foregroundColor(Color.blue)
+                .padding([.leading, .bottom, .trailing], 8.0)
+            
+            RoundedRectangle(cornerRadius: 25).stroke(lineWidth: 3).foregroundColor(Color.red).padding(8)
+        }).padding(16)
+    }
+     */
+    var body: some View {
+        ZStack(alignment: .center) {
+            Text("Hello, world!")
+                .padding([.leading, .bottom, .trailing], 8.0)
+            
+            RoundedRectangle(cornerRadius: 25).stroke(lineWidth: 3).padding(8)
         }
-        .padding()
+        .padding(16)
+        .foregroundColor(Color.red)
     }
 }
 
--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-class MemorizeApp: App {
+struct MemorizeApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
