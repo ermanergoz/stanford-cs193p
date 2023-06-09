fd78790a927f68e2ab7554bebea44be2f85afe3f
Yusuf Erman ERGÖZ <yusuf.ergoz@wundermanthompson.com>
2023-06-09 11:48:52 +0200

    1

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,20 @@
 //  Created by Yusuf Ergoz on 2023. 06. 08..
 //
 
-import Foundation
+import SwiftUI //This is a View Model so it is a part of the UI
+
+//func makeCardContent(index: Int) -> String {
+//    return "🥲"
+//}
+
+class EmojiMemoryGame {
+    //private var model: MemoryGame<String> /*We have emojis and they are strings. Here we are telling what is the type of CardContent*/ = MemoryGame<String>(numberOfPairsOfCards: 4, createContent: { index in
+    //    /*return*/ "😂" //We could also pass a function by typing makeCardContent that is declared above
+    //})
+    
+    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in "😂" } //this is a much cleaner way
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -9,15 +9,26 @@ import Foundation
 
 /*CardContent is a Generic. It is a "do not care type"*/
 struct MemoryGame<CardContent> {
-    var cards: Array<Card>
+    private(set/*other classes can look at the model but cant change it*/) var cards: Array<Card>
     
     func choose(_ card: Card) {
         
     }
     
+    init(numberOfPairsOfCards: Int, createContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        //Add numbersOfPairsOfCards x 2 cards to cards array
+        
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createContent(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
     }
 }
